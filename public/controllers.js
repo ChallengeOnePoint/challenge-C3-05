702b085c903f6fd5e2033dc15e34023856c84bb6
root <root@vps222223.ovh.net>
2016-01-23 18:42:07 +0100

    added rt creation

--- public/controllers.js
+++ public/controllers.js
@@ -1,22 +1,28 @@
 angular.module('app').controller('BoardController', function ($scope, $state, $rootScope) {
-    $scope.posts = [];
+$scope.posts = [];
     $scope.new = {};
+	function getPosts() {
     $.get({
         url: '/postits',
         success: function (res) {
-            $scope.posts = res;
+            $scope.posts = JSON.parse(res);
 	    console.log($scope.posts);
             $scope.$apply;
         }
-    })
-    $rootScope.socket.on('update_board', function (data) {
-	$scope.posts = data;
-	});
-
-    $scope.create = function () {
+    });
+}
+getPosts();
+	$rootScope.socket.on('update_board', function (data) {
+console.log("B");
+console.log(data);
+$scope.posts = data;
+});
+   
+   $scope.create = function () {
 	console.log($scope.new);
 	$rootScope.socket.emit('new_post', $scope.new);
 	$scope.new = {};
+	$rootScope.open = false;
 	}
 
     $scope.update = function (index){ };
