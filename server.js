7ce1d4b1d415698b22ee875c0a37ff22ca5e0ec1
root <root@vps222223.ovh.net>
2016-01-23 16:47:47 +0100

    init

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var postits = [];
+http.listen(4242);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/postits', function(req, res) {
+	res.send(JSON.stringify(postits));
+});
+io.on('connection', function(socket){
+	console.log('A');
+	socket.on('new_post', function(data) {
+		postits.push(data);
+		updateBoard();
+	});
+	socket.on('edit_post', function(data) {
+		postits[data.index] = data.object;
+		updateBoard();
+	});
+	socket.on('delete_post', function(index) {
+		postits.splice(index, 1);
+		updateBoard();
+	});
+
+	function updateBoard() {
+		socket.emit('update_board', postits);
+	}
+});
+app.get('/', function (req, res) {
+
+});
+
